eff32f79eed824061ecfe063a94f2104291c58a4
xavier-hcop <javihcop@gmail.com>
2019-10-03 20:05:07 -0500

    esta es la prueva

---
+++ laravel/resources/views/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <style>
+      a{
+        color: black;
+        font size: 25px; 
+
+      }
+    </style>
+    <title>Inicio</title>
+</head>
+<body style="background-color: #0ABFC8">
+<h1>Hi</h1>
+	
+  <script src="/js/jquery-3.4.1.min.js"></script>
+  <script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
